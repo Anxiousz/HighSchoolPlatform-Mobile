3f098b2a0695143de03771de4dbb5bba45386e6b
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-09-17 02:22:25 +0700

    no message

--- university_admission_portal_mobile/lib/service/authentication_service.dart
+++ university_admission_portal_mobile/lib/service/authentication_service.dart
@@ -38,22 +38,25 @@ class AuthenticationService {
 
       // Handle the response
       if (responseData['status'] == 200) {
-        // Show success message with token
-        print('Login successful: ${responseData['data']}');
-        Sharedpreferenceshelper.saveAccount(responseData['data']);
-
-        //Logout dong nay
-        // Sharedpreferenceshelper.removeInfo();
-        // Navigator.push(
-        //     context, MaterialPageRoute(builder: (context) => const HomePage()));
-        Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(
-            builder: (context) {
-              return HomePage();
-            },
-          ),
-          (route) => false,
-        );
+        if (responseData['data']['user']['role'] == 'USER') {
+          // Show success message with token
+          print('Login successful: ${responseData['data']}');
+          Sharedpreferenceshelper.saveAccount(responseData['data']);
+          //Logout dong nay
+          // Sharedpreferenceshelper.removeInfo();
+          // Navigator.push(
+          //     context, MaterialPageRoute(builder: (context) => const HomePage()));
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) {
+                return HomePage();
+              },
+            ),
+            (route) => false,
+          );
+        } else {
+          _showErrorDialog(context, 'Hệ thống chỉ dành cho người dùng', null);
+        }
       } else if (responseData['status'] == 400 ||
           responseData['status'] == 500 ||
           responseData['status'] == 404) {
